39a9c3320085a34bc6d4b3d8cab68ea135cba482
sebastiarias <Arias>
2022-06-27 23:59:20 -0500

    version 1

---
+++ suma.js
@@ -0,0 +1,7 @@
+function suma()
+{
+    var num1=Number(document.getElementById('n1').value);
+    var num2=Number(document.getElementById('n2').value);
+    var result= num1 + num2;
+    document.getElementById('result').value=result;
+}
